72567c04efca7d5fc46fd6c5b195da339233bdb1
mark newman <mark.newman@pandora.net>
2016-03-16 16:26:36 -0400

    add a button to increse presure

--- Workshop.MemoryPressure/Controllers/HomeController.cs
+++ Workshop.MemoryPressure/Controllers/HomeController.cs
@@ -8,10 +8,28 @@ namespace Workshop.MemoryPressure.Controllers
 {
     public class HomeController : Controller
     {
+        #region fields
+        private static Dictionary<Guid, byte[]> hash = new Dictionary<Guid, byte[]>();
+        private const int _pressure = 1024 * 1024 * 13;
+        #endregion
+
         [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            decimal pressure = GC.GetTotalMemory(false) / 1024 / 1024.0M;
+
+            return View(pressure);
         }
+
+        [HttpPost]
+        public ActionResult IncreasePressure()
+        {
+            hash[Guid.NewGuid()] = new byte[_pressure];
+
+            decimal pressure = GC.GetTotalMemory(false) / 1024 / 1024.0M;
+
+            return View("~/Views/Home/Index.cshtml", pressure);
+        }
+
     }
 }
