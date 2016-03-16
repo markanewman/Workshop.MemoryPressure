d9fc260683c0ef86387b011ccf1e22cbde35a291
mark newman <mark.newman@pandora.net>
2016-03-16 10:37:49 -0400

    add in simple app lifecycle logging

--- Workshop.MemoryPressure/Global.asax.cs
+++ Workshop.MemoryPressure/Global.asax.cs
@@ -1,44 +1,53 @@
 ﻿using System;
+using System.Web.Hosting;
 using System.Web.Routing;
+using Workshop.MemoryPressure.Tools;
 
 namespace Workshop.MemoryPressure
 {
     public class Global : System.Web.HttpApplication
     {
+        private const string _path = "~/Logs/log.txt";
 
         protected void Application_Start(object sender, EventArgs e)
         {
+            SimpleTextLog.WriteData(Server.MapPath(_path), "Application_Start");
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
 
         protected void Session_Start(object sender, EventArgs e)
         {
-
+            SimpleTextLog.WriteData(Server.MapPath(_path), "Session_Start-" + Session.SessionID);
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-
+            SimpleTextLog.WriteData(Server.MapPath(_path), "Application_BeginRequest-" + Request.RawUrl);
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
+            SimpleTextLog.WriteData(Server.MapPath(_path), "Application_AuthenticateRequest");
+        }
 
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            SimpleTextLog.WriteData(Server.MapPath(_path), "Application_EndRequest-" + Request.RawUrl);
         }
 
         protected void Application_Error(object sender, EventArgs e)
         {
-
+            SimpleTextLog.WriteData(Server.MapPath(_path), "Application_Error-" + Session.SessionID);
         }
 
         protected void Session_End(object sender, EventArgs e)
         {
-
+            SimpleTextLog.WriteData(Server.MapPath(_path), "Session_End-" + Session.SessionID);
         }
 
         protected void Application_End(object sender, EventArgs e)
         {
-
+            SimpleTextLog.WriteData(Server.MapPath(_path), "Application_End-" + HostingEnvironment.ShutdownReason);
         }
     }
 }
---
+++ Workshop.MemoryPressure/Tools/SimpleTextLog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace Workshop.MemoryPressure.Tools
+{
+    public static class SimpleTextLog
+    {
+        public static void WriteData(string path, string data)
+        {
+            var site = Environment.GetEnvironmentVariable("APPSETTING_WEBSITE_SITE_NAME");
+            var instance = Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID");
+            var now = DateTime.Now.ToString("yyyyMMddhhmmss");
+
+            site = site == null ? "localhost" : site;
+            instance = instance == null ? "000000" : instance.Substring(0,6);
+            
+            var line = string.Join(" ", site,instance, now, data);
+
+            using (var writer = new StreamWriter(path, true)) 
+                writer.WriteLine(line);
+        }
+    }
+}
